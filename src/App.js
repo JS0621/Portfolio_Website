df02e5ac1af22313a4dae1002cd2d6a683f88f75
Sebastian <SJazmin@student.fullsail.edu>
2022-10-20 15:05:20 -0400

    Added intro and scroll bar

--- src/App.js
+++ src/App.js
@@ -5,18 +5,25 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        {/* <img src={logo} className="App-logo" alt="logo" />  */}
+        <h3>Hello, I am</h3>
+        <br></br>
+        <h1 color='#6805F2'>Sebastian Jazmin</h1>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+        I'm a software engineer willing to take on any challenge. <br></br>
+        My main goal is to learn and improve my skill with <br></br>
+        software development. Currently I am a student<br></br>
+        working towards a bachelor's in computer science<br></br>
+        developing the FS Map mobile App. 
         </p>
-        <a
+        {/* <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
+        </a> */}
       </header>
     </div>
   );
